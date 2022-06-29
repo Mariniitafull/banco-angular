171ac442072a02947e481b79374bd3c732dce6c3
Marina <marinafullferrer@gmail.com>
2022-06-29 15:59:06 +0200

    Mi primer commit

---
+++ src/app/services/banco.service.ts
@@ -0,0 +1,27 @@
+import { Respuesta } from './../modelos/respuesta';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BancoService {
+
+  constructor() { }
+
+  async loginGestor(usuario: string, password: string): Promise<boolean> {
+
+    const response = await fetch(
+      'http://127.0.0.1:8085/login/gestor/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `usuario=${usuario}&password=${password}`
+      }
+    );
+
+    const datos: Respuesta = await response.json();
+   return datos.ok;
+
+  }
+}
